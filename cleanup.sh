43c4173e191d219697f28a5c2ed9127fee39f02b
Boris Ning <tpz7@cdc.gov>
2023-10-20 19:33:49 -0400

    Added a VERSION file to read from for single place update

--- cleanup.sh
+++ cleanup.sh
@@ -2,8 +2,25 @@
 
 set -eu
 
+file_path="VERSION"
+
+is_valid_version() {
+  [[ $1 =~ ^[0-9]+\.[0-9]+$ ]]
+}
+
+if [ -f "$file_path" ]; then
+  VERSION="$(cat "$(pwd)/$file_path")"
+  if is_valid_version "$VERSION"; then
+    echo "VERSION: $VERSION"
+  else
+    echo "Error: Invalid version format in file: $file_path"
+  fi
+else
+  echo "File not found: $file_path"
+fi
+
 IMAGE_NAME="ubuntu-22.04-cdc"
-IMAGE_VERSION="1.3"
+IMAGE_VERSION="$VERSION"
 
 # Use awk to extract container IDs into an array
 containerIDs=($(podman container ls -a | grep -i "$IMAGE_NAME:$IMAGE_VERSION" | awk '{print $1}'))
