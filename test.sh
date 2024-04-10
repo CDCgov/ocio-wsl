74c4aa73e1ff56e3039690d4e28c743d297ff2cb
Boris Ning <tpz7@cdc.gov>
2024-04-10 16:52:32 -0400

    fix: adding extra tools installation as a test

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ set -eu
 podman run -t localhost/ubuntu-22.04-cdc bash -c "python --version && which python"
 podman run -t localhost/ubuntu-22.04-cdc bash -c "python3 --version && which python3"
 podman run -t localhost/ubuntu-22.04-cdc bash -c "asdf global python 3.11.8 && python3 --version"
-# podman run -t localhost/ubuntu-22.04-cdc bash -c "bash /opt/scripts/add-extra-tools.sh"
+podman run -t localhost/ubuntu-22.04-cdc bash -c "bash /opt/scripts/add-extra-tools.sh"
