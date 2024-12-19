b9547313193c02d0300d0160afd4bff5fd07aa6e
Boris Ning <tpz7@cdc.gov>
2024-12-19 12:43:29 -0500

    fix: removing sudo usage and properly adding a new user to login

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -11,16 +11,16 @@ if getent passwd "$NEWUSER" > /dev/null; then
   echo -e "The user '$NEWUSER' already exists - skipping creation."
 else
   echo -e "The user '$NEWUSER' does not exist. Creating user with sudo permissions in Ubuntu..."
-  sudo useradd -ms /bin/bash "$NEWUSER"
-  sudo usermod -aG sudo "$NEWUSER"
-  sudo passwd -d "$NEWUSER" # Default empty password for the user
+  useradd -ms /bin/bash "$NEWUSER"
+  usermod -aG sudo "$NEWUSER"
+  passwd -d "$NEWUSER" # Default empty password for the user
 
   echo -e "User '$NEWUSER' created successfully!"
 fi
 
 # Set the user as the default for WSL
 if ! grep -q "default=${NEWUSER}" /etc/wsl.conf; then
-  echo -e "[user]\ndefault=${NEWUSER}" | sudo tee -a /etc/wsl.conf > /dev/null
+  echo -e "\n\n[user]\ndefault=${NEWUSER}" | tee -a /etc/wsl.conf > /dev/null
   echo "Default user set to '${NEWUSER}' in WSL configuration."
 else
   echo "Default user '${NEWUSER}' is already set in WSL configuration."
