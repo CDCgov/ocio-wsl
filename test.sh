6a9ddb644cdd0845a39a88aac6dfdfd74dfee129
Boris Ning <tpz7@cdc.gov>
2024-12-19 13:32:51 -0500

    fix: adding default path and testing

--- test.sh
+++ test.sh
@@ -6,3 +6,4 @@ podman run -t localhost/ubuntu-24.04-cdc bash -c "python --version && which pyth
 podman run -t localhost/ubuntu-24.04-cdc bash -c "python3 --version && which python3"
 podman run -t localhost/ubuntu-24.04-cdc bash -c "bash /opt/scripts/add-extra-tools.sh && asdf install"
 podman run -t localhost/ubuntu-24.04-cdc bash -c "uname -a && cat /etc/os-release"
+podman run -t localhost/ubuntu-24.04-cdc bash -c "ls -l /opt"
